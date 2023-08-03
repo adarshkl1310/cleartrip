3e81c8016d02ca9b109132c9999132ed1a4f67e2
adarsh kl <37417491+adarshkl1310@users.noreply.github.com>
2023-08-03 17:07:23 +0530

    Update profiles_controller.rb

--- controllers/profiles_controller.rb
+++ controllers/profiles_controller.rb
@@ -1,4 +1,19 @@
 class ProfilesController < ApplicationController
-  def show
-  end
+   before_action :set_user
+
+    def follow
+        Relationship.create_or_find_by(follower_id: current_user.id, followee_id: @user.id)
+      end
+    
+      def unfollow
+        current_user.followed_users.where(follower_id: current_user.id, followee_id: @user.id).destroy_all
+      end
+
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
 end
